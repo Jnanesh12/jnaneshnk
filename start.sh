ef6ed33ce5df7d0ded9a8ad44f8d87d88cfaf8fc
movies-universe <116450784+movies-universe@users.noreply.github.com>
2022-10-26 22:53:09 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/movies-universe /vps
+  git clone https://github.com/movies-universe 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO 
 fi
-cd /vps
+cd
 pip3 install -U -r requirements.txt
 echo "Starting Bot url...."
 python3 bot.py
