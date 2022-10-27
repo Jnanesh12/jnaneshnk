e2c785252300796e614a405ec312cfc2fda220f9
movies-universe <116450784+movies-universe@users.noreply.github.com>
2022-10-27 07:01:41 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/movies-universe 
+  git clone https://github.com/movies-universe /vps
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO 
+  git clone $UPSTREAM_REPO /vps
 fi
 cd
 pip3 install -U -r requirements.txt
